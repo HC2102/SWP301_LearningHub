82f461a897b994a2d8456f201c2952b78f448103
TruongPD <truongpd2106@gmail.com>
2023-05-15 07:58:51 +0700

    init

---
+++ src/main/java/swp391/group2/learninghub/LearningHubApplication.java
@@ -0,0 +1,13 @@
+package swp391.group2.learninghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swp391/group2/learninghub/LearningHubApplicationTests.java
@@ -0,0 +1,13 @@
+package swp391.group2.learninghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
