453829224176aa662a560dd557c252582d70d6c1
TruongPD <truongpd2106@gmail.com>
2023-06-01 11:11:28 +0700

    update flashcard controller

--- src/main/java/swp391/group2/learninghub/API/FlashcardController.java
+++ src/main/java/swp391/group2/learninghub/API/FlashcardController.java
@@ -5,12 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import swp391.group2.learninghub.Model.Feature;
-import swp391.group2.learninghub.Model.FlashcardSet;
-import swp391.group2.learninghub.Model.ResponseObject;
-import swp391.group2.learninghub.Model.User;
+import swp391.group2.learninghub.Model.*;
 import swp391.group2.learninghub.Service.FeatureService;
 import swp391.group2.learninghub.Service.FlashcardService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/flashcard")
@@ -70,6 +69,34 @@ public class FlashcardController {
 
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseObject("fail","Failed to create Flash Card Set: " + e.getMessage(),null));
+        }
+    }
+    @PostMapping("/create")
+    ResponseEntity<ResponseObject> flashCardCreate(@RequestBody Flashcard flashcard){
+        try{
+
+            flashcardService.create(flashcard);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("success","Create FlashCard successfull",flashcard)
+            );
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(
+                    new ResponseObject("fail","create fail, reason: "+e.getMessage(),null)
+            );
+        }
+    }
+    @GetMapping("/show")
+    ResponseEntity<ResponseObject> showFlashCard( @RequestParam(name="set_id",required = false,defaultValue = "")
+                                                  int set_id){
+        try{
+            List<Flashcard> fl=flashcardService.showFlashCard(set_id);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("success","Show FlashCard successfull",fl)
+            );
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(
+                    new ResponseObject("fail","Show fail, reason: "+e.getMessage(),null)
+            );
         }
     }
 }
