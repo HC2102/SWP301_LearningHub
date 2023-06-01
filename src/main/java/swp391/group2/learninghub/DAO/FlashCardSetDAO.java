7a205f1bf3d6d458084bddb86ec409f67d1f6162
TruongPD <truongpd2106@gmail.com>
2023-06-01 07:53:42 +0700

    flashCard DAO and flashcard service

--- src/main/java/swp391/group2/learninghub/DAO/FlashCardSetDAO.java
+++ src/main/java/swp391/group2/learninghub/DAO/FlashCardSetDAO.java
@@ -1,8 +1,14 @@
 package swp391.group2.learninghub.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swp391.group2.learninghub.Model.FlashcardSet;
+
+import java.util.List;
 @Repository
-public interface FlashCardSetDAO extends JpaRepository<FlashcardSet,Integer> {
+public interface FlashcardSetDAO extends JpaRepository<FlashcardSet,Integer> {
+    @Query("select s from FlashcardSet s where s.user_id= :email AND s.isActive= true")
+    public List<FlashcardSet> showUserFlashcardSetById(@Param("email")String email);
 }
--- src/main/java/swp391/group2/learninghub/Service/FlashCardServiceImpl.java
+++ src/main/java/swp391/group2/learninghub/Service/FlashCardServiceImpl.java
@@ -2,28 +2,41 @@ package swp391.group2.learninghub.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import swp391.group2.learninghub.DAO.FlashCardSetDAO;
+import swp391.group2.learninghub.DAO.FlashcardDAO;
+import swp391.group2.learninghub.DAO.FlashcardSetDAO;
 import swp391.group2.learninghub.Model.FlashcardSet;
 
-import java.sql.Date;
-import java.time.LocalDate;
+import java.util.List;
 
 @Service
-public class FlashCardServiceImpl implements FlashCardSetService {
+public class FlashcardServiceImpl implements FlashcardService{
     @Autowired
-    private FlashCardSetDAO flashCardSetDAO;
+    private FlashcardDAO flashcardDAO;
     @Autowired
-    public FlashCardServiceImpl(FlashCardSetDAO flashCardSetDAO) {
-        this.flashCardSetDAO = flashCardSetDAO;
+    private FlashcardSetDAO setDAO;
+
+    public FlashcardServiceImpl(FlashcardDAO flashcardDAO, FlashcardSetDAO setDAO) {
+        this.flashcardDAO = flashcardDAO;
+        this.setDAO = setDAO;
+    }
+
+    @Autowired
+    public String test(){
+        return "service connect success";
+    }
+    @Override
+    public List<FlashcardSet> showUserFlashcardSetById(String email) {
+        return setDAO.showUserFlashcardSetById(email);
     }
 
     @Override
-    public FlashcardSet createFlashCardSet(FlashcardSet flashCardSet) {
-        // Set the created date to the current date
-        flashCardSet.setCreatedDate(Date.valueOf(LocalDate.now()));
-        flashCardSet.setActive(true);
-        // Set other default values if needed
-        // Save the flash card set to the database
-        return flashCardSetDAO.save(flashCardSet);
+    public boolean deleteByCardById(int id) throws Exception {
+        try{
+            //find if this card is valid
+            flashcardDAO.deleteById(id);
+            return true;
+        }catch (Exception e){
+            throw new Exception("cannot delete card, reason: "+e.getMessage());
+        }
     }
 }
