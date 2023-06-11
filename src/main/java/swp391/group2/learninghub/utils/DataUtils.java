8c63eba5a6b65d7654320f6241cfbc134dada509
An Phan <thanhanphan114@gmail.com>
2023-06-12 04:28:53 +0700

    reused random object

--- src/main/java/swp391/group2/learninghub/utils/DataUtils.java
+++ src/main/java/swp391/group2/learninghub/utils/DataUtils.java
@@ -3,17 +3,19 @@ package swp391.group2.learninghub.utils;
 import java.util.Random;
 
 public class DataUtils {
+    private static Random r = new Random();
+
     public static String generateTempPwd(int length) {
         String numbers;
-        numbers ="012345678";
-        char[] otp =new char[length];
-        Random r=new Random();
-        for(int i=0;i<length;i++) {
-            otp[i]=numbers.charAt(r.nextInt(numbers.length()));
+        numbers = "012345678";
+        char[] otp = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            otp[i] = numbers.charAt(r.nextInt(numbers.length()));
         }
-        String optCode="";
-        for(int i=0;i<otp.length;i++) {
-            optCode+=otp[i];
+        String optCode = "";
+        for (int i = 0; i < otp.length; i++) {
+            optCode += otp[i];
         }
         return optCode;
     }
