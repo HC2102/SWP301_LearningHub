c4e67347505e4149da015066a79e13268e7fbd02
TranHuy103258 <huy.tranquang02@gmail.com>
2023-06-12 09:11:35 +0700

    SG2L-21

--- src/main/java/swp391/group2/learninghub/utils/DataUtils.java
+++ src/main/java/swp391/group2/learninghub/utils/DataUtils.java
@@ -1,5 +1,7 @@
 package swp391.group2.learninghub.utils;
 
+import org.apache.logging.log4j.util.StringBuilderFormattable;
+
 import java.util.Random;
 
 public class DataUtils {
@@ -13,10 +15,10 @@ public class DataUtils {
         for (int i = 0; i < length; i++) {
             otp[i] = numbers.charAt(r.nextInt(numbers.length()));
         }
-        String optCode = "";
+        StringBuilder bld = new StringBuilder();
         for (int i = 0; i < otp.length; i++) {
-            optCode += otp[i];
+            bld.append(otp[i]);
         }
-        return optCode;
+        return bld.toString();
     }
 }
