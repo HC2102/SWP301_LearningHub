6caa56ebd1a0bb3b523e976b02db4fc2635d1fcb
An Phan <thanhanphan114@gmail.com>
2023-06-14 21:09:11 +0700

    api to show all notes

--- src/main/java/swp/group2/learninghub/dao/NoteDAO.java
+++ src/main/java/swp/group2/learninghub/dao/NoteDAO.java
@@ -1,8 +1,15 @@
 package swp.group2.learninghub.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swp.group2.learninghub.model.Note;
+
+import java.util.List;
+
 @Repository
 public interface NoteDAO extends JpaRepository<Note,Integer> {
+    @Query("SELECT n FROM Note n where n.userId=:email")
+    public List<Note> showUserNotesByEmail(@Param("email") String email);
 }
