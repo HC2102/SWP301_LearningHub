ffd0538ba6b8bc08ced10fc7c3eefe132ef2e585
Lehunglong <139702126+Lehunglong@users.noreply.github.com>
2023-07-17 20:48:28 +0700

    Delete src directory

--- src/main/java/swp/group2/learninghub/dao/LabelDAO.java
+++
@@ -1,8 +0,0 @@
-package swp.group2.learninghub.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import swp.group2.learninghub.model.BoardLabel;
-@Repository
-public interface LabelDAO extends JpaRepository<BoardLabel,Integer> {
-}
