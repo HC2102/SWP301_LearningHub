d5abb1b10c7cb4100ffef1ddf61ebe29c7684044
Lehunglong <139702126+Lehunglong@users.noreply.github.com>
2023-07-17 20:49:58 +0700

    Delete src/main directory
    e

--- src/main/java/swp/group2/learninghub/dao/FeatureDAO.java
+++
@@ -1,9 +0,0 @@
-package swp.group2.learninghub.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import swp.group2.learninghub.model.Feature;
-@Repository
-public interface FeatureDAO extends JpaRepository<Feature,Integer> {
-}
--- src/main/java/swp/group2/learninghub/service/MailService.java
+++
@@ -1,8 +0,0 @@
-package swp.group2.learninghub.service;
-
-import jakarta.mail.MessagingException;
-import swp.group2.learninghub.model.DataMailDTO;
-
-public interface MailService {
-    void sendHtmlMail(DataMailDTO dataMail, String templateName) throws MessagingException;
-}
